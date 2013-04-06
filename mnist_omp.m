abb003c627d11db0177c03293c527d85deb95877
Cameron P.H. Chen <Cameron.Chenx@gmail.com>
2013-04-06 11:36:04 -0400

    modify the iteration number for omp add TODO.txt add Iterative Dome Test : IDT.m

--- mnist_omp.m
+++ mnist_omp.m
@@ -1,12 +1,57 @@
 % run omp on mnist data
+% get 5000 balanced sample from training data, sample without replacment
+% get 60 balanced sample from testing data, sample without replacment
 % by Cameron P.H. Chen @ Princeton
 
+clear all
 
-images = loadMNISTImages('/Users/ChimatChen/Dropbox/Research/OMPvsScreening/data/train-images-idx3-ubyte');
-labels = loadMNISTLabels('/Users/ChimatChen/Dropbox/Research/OMPvsScreening/data/train-labels-idx1-ubyte');
-images = loadMNISTImages('/Users/ChimatChen/Dropbox/Research/OMPvsScreening/data/t10k-images-idx1-ubyte');
-labels = loadMNISTLabels('/Users/ChimatChen/Dropbox/Research/OMPvsScreening/data/t10k-labels-idx1-ubyte');
+trainSampleNum = 5000;
+testSampleNum = 60;
 
-w_omp = omp(B,x,0,0)
+train_images = loadMNISTImages('/Users/ChimatChen/Dropbox/Research/OMPvsScreening/data/train-images-idx3-ubyte');
+train_labels = loadMNISTLabels('/Users/ChimatChen/Dropbox/Research/OMPvsScreening/data/train-labels-idx1-ubyte');
+test_images = loadMNISTImages('/Users/ChimatChen/Dropbox/Research/OMPvsScreening/data/t10k-images-idx3-ubyte');
+test_labels = loadMNISTLabels('/Users/ChimatChen/Dropbox/Research/OMPvsScreening/data/t10k-labels-idx1-ubyte');
 
+% balanced sampling from training and testing dataset
+
+train_image_sample = nan(size(train_images,1),trainSampleNum);
+test_image_sample = nan(size(test_images,1),testSampleNum);
+train_labels_sample = nan(trainSampleNum,1);
+test_labels_sample = nan(testSampleNum,1);
+
+test_distri = nan(10,1);
+train_distri = nan(10,1);
+
+
+for i=1:10
+  test_distri(i,:) = sum(test_labels==(i-1));
+  train_distri(i,:) = sum(train_labels==(i-1));
+end
+
+for i=1:10
+  tmp_test_idc=find(test_labels==i-1);
+  tmp_train_idc=find(train_labels==i-1);
+
+  test_image_sample(:,(i-1)*(testSampleNum/10)+1:i*(testSampleNum/10))=...
+        test_images(:,tmp_test_idc(randsample(test_distri(i,:),testSampleNum/10)));
+
+  train_image_sample(:,(i-1)*(trainSampleNum/10)+1:i*(trainSampleNum/10))=...
+        train_images(:,tmp_train_idc(randsample(train_distri(i,:),trainSampleNum/10)));
+
+  test_labels_sample((i-1)*(testSampleNum/10)+1:i*(testSampleNum/10))=...
+        test_labels(tmp_test_idc(randsample(test_distri(i,:),testSampleNum/10)));
+
+  train_labels_sample((i-1)*(trainSampleNum/10)+1:i*(trainSampleNum/10))=...
+        train_labels(tmp_train_idc(randsample(train_distri(i,:),trainSampleNum/10)));
+end 
+
+L2error=nan(60,1);
+
+for i=1:60
+  fprintf('i:%d\n',i);
+  %%check the stopping criteria of OMP
+  w=omp(train_image_sample,test_image_sample(:,i),0,0,30);
+  L2error(i,:)=norm(train_image_sample*w-test_image_sample(:,i),2)/norm(test_image_sample(:,i),2);
+end
 
--- omp.m
+++ omp.m
@@ -5,24 +5,26 @@
 %     weight: w
 % using pseudoinverse to handle all kinds of singularity problem when projecting
 % signal on the selected dictionary
+% by Cameron P.H. Chen @ Princeton
 
-function [w]=omp(B,x,e0,k0)
+function [w]=omp(B,x,e0,k0,iter)
+
+assert(size(B,1)==size(x,1), 'dimension of B and x must agree')
 
 % initialization
 n=size(B,1);
 m=size(B,2);
-w=zeros(m,n);
+w=zeros(m,1);
 r=x;
 e=norm(r,2);
 B1=zeros(size(B));
 SS=[];
-for k=1:1:n 
+for k=1:1:iter
   [value idc] =max(abs(B'*r));
   SS=sort([SS,idc]);
-  w(:,k)=zeros(m,1);
-  w(SS,k)= pinv(B(:,SS))*x;
-  r = x-B(:,SS)*w(SS,k);
+  w=zeros(m,1);
+  w(SS)= pinv(B(:,SS))*x;
+  r = x-B(:,SS)*w(SS);
   e=norm(r,2);
-%  k=k+1;
 end
 
