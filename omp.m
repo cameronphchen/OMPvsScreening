502d4c0f90beaa998b3e1536cd15289ea3059e37
Cameron P.H. Chen <Cameron.Chenx@gmail.com>
2013-04-06 00:02:08 -0400

    orthogonal matching pursuit first version

---
+++ omp.m
@@ -0,0 +1,26 @@
+% This program implement the OMP 
+% Input:
+%     dictionary: B; signal: x; error threshold: e; max number of iteration: kmax
+% Output:
+%     weight: w
+
+function [w]=omp(B,x,e0,k0)
+
+% initialization
+n=size(B,1);
+m=size(B,2);
+w=zeros(m,n);
+r=x;
+e=norm(r,2);
+B1=zeros(size(B));
+SS=[];
+for k=1:1:n 
+  [value idc] =max(abs(B'*r));
+  SS=sort([SS,idc]);
+  w(:,k)=zeros(m,1);
+  w(SS,k)= pinv(B(:,SS))*x;
+  r = x-B(:,SS)*w(SS,k);
+  e=norm(r,2);
+%  k=k+1;
+end
+
