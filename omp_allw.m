1bd7a6054f106514e41370a9ddbb9beaee5bd0a8
Cameron P.H. Chen <Cameron.Chenx@gmail.com>
2013-04-06 10:56:47 -0400

    add unit test for omp: test_omp.m modify omp output as 1 vector omp that output all the w along the way are called omp_allw.m working version of mnist_omp.m

---
+++ omp_allw.m
@@ -0,0 +1,29 @@
+% This program implement the OMP 
+% Input:
+%     dictionary: B; signal: x; error threshold: e; max number of iteration: kmax
+% Output:
+%     weight: w
+% using pseudoinverse to handle all kinds of singularity problem when projecting
+% signal on the selected dictionary
+% by Cameron P.H. Chen @ Princeton
+
+function [w]=omp(B,x,e0,k0,iter)
+
+% initialization
+n=size(B,1);
+m=size(B,2);
+w=zeros(m,n);
+r=x;
+e=norm(r,2);
+B1=zeros(size(B));
+SS=[];
+for k=1:1:iter
+  [value idc] =max(abs(B'*r));
+  SS=sort([SS,idc]);
+  w(:,k)=zeros(m,1);
+  w(SS,k)= pinv(B(:,SS))*x;
+  r = x-B(:,SS)*w(SS,k);
+  e=norm(r,2);
+%  k=k+1;
+end
+
---
+++ test_omp.m
@@ -0,0 +1,33 @@
+% unit test for omp
+% simple toy example
+% assume dictionary is unit norm
+% by Cameron P.H. Chen @ Princeton
+
+
+%simple toy exmaple
+B = [1 0 0 0;
+     0 1 0 0.707;
+     0 0 1 0.707;]
+
+x = [1 2 3]';
+
+w=omp(B,x,0,0,size(B,1))
+assert(norm(w-[1 0 1 2.8289]',2)<0.0001);
+
+
+
+%singular B matrix
+B = [0 0 0 0;
+     0 0 0 0.707;
+     0 0 1 0.707;]
+
+x = [0 2 3]';
+
+w=omp(B,x,0,0,size(B,1))
+
+assert(norm(w-[0 0 1 1.4144]',2)<0.0001);
+
+
+
+
+fprintf('Pass omp unit test\n')
